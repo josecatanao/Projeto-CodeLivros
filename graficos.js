38acc0379addcea50cb5025a11dac7f80ec6f2b5
Wendel Alves Pereira <wendel.alves@academico.ifpb.edu.br>
2020-02-15 23:07:33 -0300

    adicionando os graficos e modificando o design

---
+++ graficos.js
@@ -0,0 +1,103 @@
+//Grafico de barras dos livros de Literatura;
+
+var graficoBarraLi= document.getElementById("GraficoBarraLI");
+
+var x = new Chart(graficoBarraLi,{
+    type: 'bar',
+    data: {
+        labels: ['Livro1', 'Livro 2', 'Livro 3'],
+        datasets: [{
+            label: 'Livros Mais Vendidos de Literatura',
+            data: [300, 400, 600],
+            backgroundColor: ['rgba(255, 99, 132, 1)','rgba(54, 162, 235, 1)','rgba(255, 206, 86, 1)'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+    }
+});
+
+
+var graficoPizzaLi = document.getElementById("GraficosPizzaLI");
+
+var y = new Chart(graficoPizzaLi,{
+    type: 'pie',
+    data: {
+        labels: ['Livro1', 'Livro 2', 'Livro 3'],
+        datasets: [{
+            label: 'Livros Mais Vendidos de Literatura',
+            data: [300, 400, 600],
+            backgroundColor: ['rgba(255, 99, 132, 1)','rgba(54, 162, 235, 1)','rgba(255, 206, 86, 1)'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+//graficos de barras dos livros de Informatica
+
+var graficoBarraTI= document.getElementById("GraficoBarraTI");
+
+var x = new Chart(graficoBarraTI,{
+    type: 'bar',
+    data: {
+        labels: ['Livro1', 'Livro 2', 'Livro 3'],
+        datasets: [{
+            label: 'Livros Mais Vendidos de Informatica',
+            data: [300, 400, 600],
+            backgroundColor: ['rgba(255, 99, 132, 1)','rgba(54, 162, 235, 1)','rgba(255, 206, 86, 1)'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+    }
+});
+
+//grafico de pizza dos livros de informatica
+
+var graficoPizzaTI = document.getElementById("GraficosPizzaTI");
+
+var y = new Chart(graficoPizzaTI,{
+    type: 'pie',
+    data: {
+        labels: ['Livro1', 'Livro 2', 'Livro 3'],
+        datasets: [{
+            label: 'Livros Mais Vendidos de Literatura',
+            data: [300, 400, 600],
+            backgroundColor: ['rgba(255, 99, 132, 1)','rgba(54, 162, 235, 1)','rgba(255, 206, 86, 1)'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
