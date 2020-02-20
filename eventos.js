7d83dbc997b0339b73e139c2dac28c2c7a558c5f
josecatanao <51245690+josecatanao@users.noreply.github.com>
2020-02-20 09:59:22 -0300

    add eventos modal

--- eventos.js
+++ eventos.js
@@ -1,5 +1,28 @@
-function mudar(){
-        var element = document.getElementById("abrirModal1");
-        element.classList.add(".modal");
-      
+function mudar1(){
+        var a = document.getElementById('li1'); 
+        a.href = "#abrirModal1"
+}
+function mudar2(){
+        var a = document.getElementById('li2'); 
+        a.href = "#abrirModal2"
+}
+function mudar3(){
+        var a = document.getElementById('li3'); 
+        a.href = "#abrirModal3"
+}
+
+
+
+
+function mudar4(){
+        var a = document.getElementById('li4'); 
+        a.href = "#abrirModal4"
+}
+function mudar5(){
+        var a = document.getElementById('li5'); 
+        a.href = "#abrirModal5"
+}
+function mudar6(){
+        var a = document.getElementById('li6'); 
+        a.href = "#abrirModal6"
 }
